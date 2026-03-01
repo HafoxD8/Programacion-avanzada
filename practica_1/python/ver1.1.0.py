3dfe18f24b53ab399a5ec9ae1d79e4a9018dd15a
tyrannosauruspapuh-wq <tyrannosauruspapuh@gmail.com>
2026-02-28 23:43:23 -0600

    Add files via upload

---
+++ practica_1/python/ver1.1.0.py
@@ -0,0 +1,13 @@
+# Programa: Cálculo de resistencia total
+# Autores: Cruz Molina Hafid
+#          Hernández Fuentes Nazario
+#          Méndez García Frank Asael
+#          Reyes Casanova Luis Khaled
+#          Yáñez Mancilla Jesús Osvaldo
+# Versión: 1.1.0 - Corregido por: 
+# Cambio: Se agrega entrada en de valores por teclado
+
+R1=float(input("Valor de R1: "))
+R2=float(input("Valor de R2: "))
+RT=(R1*R2)/(R1+R2)
+print("La resistencia total es: ", RT)
