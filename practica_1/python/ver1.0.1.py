86471aad68053ae151040eb876489f2eb202265b
Frank Armed <frank4rmstrong01@gmail.com>
2026-02-28 23:11:56 -0600

    Update ver1.0.1.py

--- practica_1/python/ver1.0.1.py
+++ practica_1/python/ver1.0.1.py
@@ -5,9 +5,10 @@
 #          Reyes Casanova Luis Khaled
 #          Yáñez Mancilla Jesús Osvaldo
 # Versión: 1.0.1 - 28-02-2026 - Corregido por: Frank Asael Méndez García
-# Cambio: Se corrige el cálculo de resistencia en paralelo
+# Cambio: Se corrige el cálculo de resistencia en paralelo 
 
 R1=6
 R2=8
 RT=(R1*R2)/(R1+R2)
+
 print("La resistencia total es: ", RT)
