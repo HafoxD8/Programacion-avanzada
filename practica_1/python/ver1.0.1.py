2d128b64f980b504a08e59284597a661a1e4152e
Frank Armed <frank4rmstrong01@gmail.com>
2026-02-28 23:11:06 -0600

    Add files via upload
    Versión 1.0.1 del código en Python
    Cambio: Se corrige el cálculo de resistencia en paralelo

---
+++ practica_1/python/ver1.0.1.py
@@ -0,0 +1,13 @@
+# Programa: Cálculo de resistencia total
+# Autores: Cruz Molina Hafid
+#          Hernández Fuentes Nazario
+#          Méndez García Frank Asael
+#          Reyes Casanova Luis Khaled
+#          Yáñez Mancilla Jesús Osvaldo
+# Versión: 1.0.1 - 28-02-2026 - Corregido por: Frank Asael Méndez García
+# Cambio: Se corrige el cálculo de resistencia en paralelo
+
+R1=6
+R2=8
+RT=(R1*R2)/(R1+R2)
+print("La resistencia total es: ", RT)
