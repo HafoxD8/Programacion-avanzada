93664eb4334ba776a03e62480214597b547f7cf1
tyrannosauruspapuh-wq <tyrannosauruspapuh@gmail.com>
2026-02-28 23:45:17 -0600

    Update ver1.1.0.py

--- practica_1/python/ver1.1.0.py
+++ practica_1/python/ver1.1.0.py
@@ -4,10 +4,11 @@
 #          Méndez García Frank Asael
 #          Reyes Casanova Luis Khaled
 #          Yáñez Mancilla Jesús Osvaldo
-# Versión: 1.1.0 - Corregido por: 
+# Versión: 1.1.0 - Corregido por: Yáñez Mancilla Jesús Osvaldo
 # Cambio: Se agrega entrada en de valores por teclado
 
 R1=float(input("Valor de R1: "))
 R2=float(input("Valor de R2: "))
 RT=(R1*R2)/(R1+R2)
+
 print("La resistencia total es: ", RT)
