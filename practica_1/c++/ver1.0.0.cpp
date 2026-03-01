033811fe4fcd5fecabc5513dc106815a420c1b17
Frank Armed <frank4rmstrong01@gmail.com>
2026-02-28 23:05:24 -0600

    Add files via upload
    Versión 1.0.0 del código de Python en c++

---
+++ practica_1/c++/ver1.0.0.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Programa: Cálculo de resistencia total en paralelo
+// Autores: Cruz Molina Hafid
+//          Hernández Fuentes Nazario
+//          Méndez García Frank Asael
+//          Reyes Casanova Luis Khaled
+//          Yáñez Mancilla Jesús Osvaldo
+// Versión: 1.0.0 – 2026-02-28 – Frank Asael Méndez García Version del codigo inicial 1.0.0 de python en c++.
+// Descripción: Calcular la resistencia total con 2 resistencias en paralelo
+
+int main() {
+    int R1 = 6;
+    int R2 = 8;
+    int RT = R1 + R2;
+    printf("La resistencia total es: %d\n", RT);
+    return 0;
+}
