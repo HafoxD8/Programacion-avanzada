393d79a32e2e4b546faeff0e3e5a3e61561960ad
SNPAIX <freddybethoven@gmail.com>
2026-02-28 23:54:50 -0600

    Add files via upload

---
+++ practica_1/python/ver1.0.0.py
@@ -0,0 +1,13 @@
+# Programa: Cálculo de resistencia total en paralelo
+# Autores: Cruz Molina Hafid
+#          Hernández Fuentes Nazario
+#          Méndez García Frank Asael
+#          Reyes Casanova Luis Khaled
+#          Yáñez Mancilla Jesús Osvaldo
+# Versión: 1.0.0 - 28-02-2026 - Corregido por: Reyes Casanova Luis Khaled
+# Descripción: Calcular la resistencia total con 2 resistencias en paralelo
+
+R1=6
+R2=8
+RT=R1+R2
+print("La resistencia total es: ", RT)
